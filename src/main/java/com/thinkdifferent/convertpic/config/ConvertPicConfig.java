fe1ad1f42f9ba4bfe1a65d84f8dcc3c5eaea17ac
zhangzhen1979 <zhen.zhang@163.com>
2021-12-22 17:00:38 +0800

    加入Nacos支持

--- src/main/java/com/thinkdifferent/convertpic/config/ConvertPicConfig.java
+++ src/main/java/com/thinkdifferent/convertpic/config/ConvertPicConfig.java
@@ -1,9 +1,11 @@
 package com.thinkdifferent.convertpic.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 public class ConvertPicConfig {
 
     public static String inPutTempPath;
